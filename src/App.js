c39108bcdfe634c00bb03d8a982b0d960f4f5e77
Rose Ann Austria <roseann1024@gmail.com>
2024-02-22 11:28:53 -0700

    Created footer in the landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App Created by React</h1>
+      <footer>
+        This was coded by <a href="/">Rose Ann V. Austria</a> and open-sourced
+        in <a href="/">Git-Hub.</a>
+      </footer>
     </div>
   );
 }
-
-export default App;
